f6c6e0dcf7a508f6e9eb02a1e2c09bee87bc0bd8
seung1 <97tmddnjs@naver.com>
2024-11-06 00:00:15 +0900

    feat: 메인페이지 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,41 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <header className={styles.header}>Seungwon's simple site</header>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <main className={styles.main}>
+        <h1>세상에서 가장 간단한 사이트입니다</h1>
+        <h2>
+          <summary>일단 빠르게 검색이 되게하는게 목표에요.</summary>
+        </h2>
+        <article>
+          <ol>
+            <li>이게 메인페이지에요.</li>
+            <li>아무 기능이 없습니다.</li>
+            <li>얼마나 빠르게 검색엔진에 올라갈지 궁금합니다.</li>
+            <li>
+              너무 내용이 빈약하면 잘 안올라간다고해서 내용을 좀 채워줍니다.
+            </li>
+            <li>첫번째 시도라서 사진은 넣지 않겠습니다. </li>
+          </ol>
+        </article>
+        <div>
+          <h3>개발</h3>
+          <h3>웹사이트</h3>
+          <h3>테스트</h3>
+          <h3>검색엔진</h3>
+          <h3>구글</h3>
+          <h3>SEO</h3>
+          <h3>가장 간단한</h3>
+          <h3>검색이 되게</h3>
         </div>
+
+        <div className={styles.ctas}></div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div>만든이 : Seungwon</div>
       </footer>
     </div>
   );
